41d40fd6986359eb179b31514038b68ad6817229
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-11-30 02:10:19 +0330

    fix(crypto/demo): slice user id

--- demo/crypto/many-file.ts
+++ demo/crypto/many-file.ts
@@ -12,7 +12,7 @@ const userFactory = new AlwatrUserGenerator({
 
 for (let i = 10_000; i > 0; i--) {
   const id = userFactory.generateUserId();
-  const id2 = id.slice(0, 3) + '/' + id.slice(3);
+  const id2 = id.slice(0, 3) + '/' + id;
   const data = {id, hello: 'world'};
   const fileName1 = `${process.env.HOME}/temp1/${id}.json`;
   const fileName2 = `${process.env.HOME}/temp2/${id2}.json`;
