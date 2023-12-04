52343ead04c23f50bedac2caa01f46bf489ab318
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-12-04 13:08:28 +0330

    feat(crypto): rename user file to api

--- packages/crypto/src/index.ts
+++ packages/crypto/src/index.ts
@@ -2,7 +2,7 @@ import {definePackage} from '@alwatr/logger';
 
 export * from './hash.js';
 export * from './token.js';
-export * from './user.js';
+export * from './api.js';
 export * from './type.js';
 export * from './pre-config.js';
 
