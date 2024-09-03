d0b81fa41d39cf591bd7c0f9cbbddb851550922c
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-09-03 19:18:06 +0330

    refactor: reset repo base for starting nanotron

--- packages/crypto/src/index.ts
+++
@@ -1,9 +0,0 @@
-import {definePackage} from '@alwatr/logger';
-
-export * from './hash.js';
-export * from './token.js';
-export * from './api.js';
-export * from './type.js';
-export * from './pre-config.js';
-
-definePackage('crypto', '3.x');
