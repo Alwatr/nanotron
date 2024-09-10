fe94be388d7f97b84ee0878595731e6f85886e6e
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-09-10 18:59:51 +0330

    feat: Add NanotronApiServerConfig interface

---
+++ packages/api-server/src/api-server.ts
@@ -0,0 +1,63 @@
+
+/**
+ * Configuration options for the NanotronApiServer.
+ */
+export interface NanotronApiServerConfig {
+  /**
+   * The port number to listen on.
+   *
+   * @default 80
+   */
+  port?: number;
+
+  /**
+   * The hostname to listen on.
+   *
+   * @default '0.0.0.0'
+   */
+  host?: string;
+
+  /**
+   * Sets the timeout (ms) for receiving the entire request from the client.
+   *
+   * @default 10_000 ms
+   */
+  requestTimeout?: number;
+
+  /**
+   * Sets the timeout (ms) for receiving the complete HTTP headers from the client.
+   *
+   * This should be bigger than `keepAliveTimeout + your server's expected response time`.
+   *
+   * @default 130_000 ms
+   */
+  headersTimeout?: number;
+
+  /**
+   * Sets the timeout (ms) for receiving the complete HTTP headers from the client.
+   *
+   * @default 120_000 ms
+   */
+  keepAliveTimeout?: number;
+
+  /**
+   * Add /health route.
+   *
+   * @default true
+   */
+  healthRoute?: boolean;
+
+  /**
+   * Add OPTIONS route for preflight requests to allow access all origins.
+   *
+   * @default false
+   */
+  allowAllOrigin?: boolean;
+
+  /**
+   * A prefix to be added to the beginning of the `url` of all defined routes.
+   *
+   * @default '/api/'
+   */
+  prefix?: `/${string}/` | '/';
+}
