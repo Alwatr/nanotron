c94c9546927c13b3b72d24522bd1fc46ce4f6163
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-09-12 12:00:36 +0330

    refactor(api-server): Update main.ts exports with NanotronClientRequest, NanotronServerResponse, and NanotronUrl types

--- packages/api-server/src/main.ts
+++ packages/api-server/src/main.ts
@@ -1,5 +1,6 @@
 export * from './api-server.js';
-export * from './api-client-request.js';
-export * from './api-server-response.js';
+export type {NanotronClientRequest} from './api-client-request.js';
+export type {NanotronServerResponse} from './api-server-response.js';
+export type {NanotronUrl} from './url.js';
 export * from './type.js';
 export * from './const.js';
