169f36053c9ff94192e7126695859ff5e4e275b9
Mohammad Honarvar <honarvar.info@gmail.com>
2024-09-29 17:59:35 +0330

    feat(nanotron): install `nanolib` packages & export them here

--- packages/nanotron/src/main.ts
+++ packages/nanotron/src/main.ts
@@ -1,8 +1,23 @@
-import {definePackage} from '@alwatr/logger';
-
-import type {} from '@alwatr/nano-build';
+import {packageTracer} from '@alwatr/package-tracer';
+import '@alwatr/polyfill-has-own';
 
 export * from '@alwatr/nanotron-api-server';
 export * from '@alwatr/crypto';
 
-definePackage('@alwatr/nanotron', __package_version__);
+export * from '@alwatr/async-queue';
+export * from '@alwatr/delay';
+export * from '@alwatr/global-scope';
+export * from '@alwatr/logger';
+export * from '@alwatr/package-tracer';
+export * from '@alwatr/dedupe';
+export * from '@alwatr/flatomise';
+export * from '@alwatr/is-number';
+export * from '@alwatr/parse-duration';
+export * from '@alwatr/deep-clone';
+export * from '@alwatr/exit-hook';
+export * from '@alwatr/flat-string';
+export * from '@alwatr/local-storage';
+export * from '@alwatr/node-fs';
+export * from '@alwatr/platform-info';
+
+packageTracer.add(__package_name__, __package_version__);
