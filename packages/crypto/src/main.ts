cf1a48071fe745226f6aed2afdf11038da5065e2
Mohammad Honarvar <honarvar.info@gmail.com>
2024-09-29 18:03:38 +0330

    fix(crypto): update types & `logger` & use `packageTracer` based on last changes of `nanolib`

--- packages/crypto/src/main.ts
+++ packages/crypto/src/main.ts
@@ -1,11 +1,5 @@
-import {definePackage} from '@alwatr/logger';
-
-import type {} from '@alwatr/nano-build';
-
 export * from './hash.js';
 export * from './token.js';
 export * from './api.js';
 export * from './type.js';
 export * from './pre-config.js';
-
-definePackage('@alwatr/crypto', __package_version__);
