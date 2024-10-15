9448b936fab4abeae02a6b0a2f413cdf73d0eeb5
MohammadHonarvar <honarvar.info@gmail.com>
2024-10-15 21:34:35 +0330

    feat(nanotron): separate `export`s based on the `package`s name

--- packages/nanotron/src/main.ts
+++ packages/nanotron/src/main.ts
@@ -1,6 +1,3 @@
 import {packageTracer} from '@alwatr/nanolib';
 
-export * from '@alwatr/nanotron-api-server';
-export * from '@alwatr/crypto';
-
 __dev_mode__: packageTracer.add(__package_name__, __package_version__);
